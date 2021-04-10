f1c0be23946498ac571bb20b2ef1f0379f2f1027
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2021-04-10 19:53:56 -0400

    Delete Pinner.d.ts

--- dist/lib/utility/Pinner.d.ts
+++
@@ -1,7 +0,0 @@
-import Eris from "eris";
-declare class Pinner {
-    _pinMessage(message: Eris.Message): void;
-    _unPinMessage(message: Eris.Message): void;
-}
-declare const _default: Pinner;
-export default _default;
